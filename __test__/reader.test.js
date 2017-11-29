12d1dc7947ab9dec45e5da19f21f5eedd43e5d11
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-11-29 14:43:04 -0800

    reformatting method argument input so that first argument is array of paths instead of single path

--- __test__/reader.test.js
+++ __test__/reader.test.js
@@ -4,9 +4,18 @@ const reader = require(`../lib/reader`);
 
 describe(`reader.js`, () => {
   test(`reader should take in an array of three file paths and return an array of three strings if no errors are present`, (done) => {
-    reader.readFile((error, output) => {
-      expect(error).toBeNull();
-      done();
-    });
+    reader.readFile([], 
+      (error, output) => {
+        expect(error).toBeNull();
+        expect(typeof output).toBe(`string`);
+        done();
+      }
+    );
   });
 });
+
+// describe(`this file`, () => {
+//   test(`some function does what I think`, () => {
+//     expect(object.method(inputs)).toBe(expected output);
+//   })
+// })
