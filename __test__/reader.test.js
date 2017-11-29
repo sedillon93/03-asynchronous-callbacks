ea52c7d63c6c6671606707af8221b1410e06fb61
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-11-29 13:34:16 -0800

    write first test to make sure no error is thrown

--- __test__/reader.test.js
+++ __test__/reader.test.js
@@ -3,7 +3,16 @@
 const reader = require(`../lib/reader`);
 
 describe(`reader.js`, () => {
-  test(`reader should take in an array of three file paths and return an array of three strings if no errors are present`, () => {
-    
-  })
+  test(`reader should take in an array of three file paths and return an array of three strings if no errors are present`, (done) => {
+    reader.readFile((error, output) => {
+      expect(error).toBeNull();
+      done();
+    });
+  });
 });
+
+// describe(`this file`, () => {
+//   test(`some function does what I think`, () => {
+//     expect(object.method(inputs)).toBe(expected output);
+//   })
+// })
